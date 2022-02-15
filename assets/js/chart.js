abe4c91753e651a7de137872233ef0a32e386087
Nouira Alim <nouiraalim@gmail.com>
2022-02-15 12:39:26 +0100

    Export du HTML, CSS et Javascript du PHP

---
+++ assets/js/chart.js
@@ -0,0 +1,37 @@
+function changeYear()
+{
+    const year = parseInt(document.querySelector('select#years').value);
+    
+    myChart.data.datasets[0].label = 'Number of deaths in ' + year;
+    myChart.data.datasets[0].data = values[year];
+    myChart.update();
+
+}
+
+const ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: 'Number of deaths in 2010',
+            data:values[2010],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+console.log(myChart);
--- index.php
+++ index.php
@@ -1,21 +1,22 @@
-<style>
-    table {
-        border-collapse: collapse;
-    }
+<link rel="stylesheet" href="assets/css/chart.css">
 
-    th, td {
-        border: 1px solid black;
-        padding: 10px;
-    }
+<canvas id='myChart'></canvas>
+<select range='years' id='years' onchange='changeYear()'>
+    <option value='2010' selected> 2010 </option>
+    <option value='2011'> 2011 </option>
+    <option value='2012'> 2012 </option>
+    <option value='2013'> 2013 </option>
+    <option value='2014'> 2014 </option>
+    <option value='2015'> 2015 </option>
+    <option value='2016'> 2016 </option>
+    <option value='2017'> 2017 </option>
+    <option value='2018'> 2018 </option>
+    <option value='2019'> 2019 </option>
+    <option value='2020'> 2020 </option>
+    <option value='2021'> 2021 </option>
+</select>
 
-    canvas#myChart {
-        width: 400px;
-        height: 400px;
-    }
-</style>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
-
- 
 
 <?php
 
@@ -41,63 +42,11 @@ while (!feof($handle))
 
 fclose($handle);
 
-echo "<canvas id='myChart'></canvas>
-<select range='tears' id='years' onchange='changeYear()'>
-    <option value='2010' selected> 2010 </option>
-    <option value='2011'> 2011 </option>
-    <option value='2012'> 2012 </option>
-    <option value='2013'> 2013 </option>
-    <option value='2014'> 2014 </option>
-    <option value='2015'> 2015 </option>
-    <option value='2016'> 2016 </option>
-    <option value='2017'> 2017 </option>
-    <option value='2018'> 2018 </option>
-    <option value='2019'> 2019 </option>
-    <option value='2020'> 2020 </option>
-    <option value='2021'> 2021 </option>
-</select>
+echo "
 <script>
-
-function changeYear()
-{
-    const year = parseInt(document.querySelector('select#years').value);
-    
-    myChart.data.datasets[0].label = 'Number of deaths in ' + year;
-    myChart.data.datasets[0].data = values[year];
-    myChart.update();
-
-}
-
 const values = " . json_encode($deaths) . "
-
-
-const ctx = document.getElementById('myChart').getContext('2d');
-let myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        datasets: [{
-            label: 'Number of deaths in 2010',
-            data:values[2010],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-
-            ],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-console.log(myChart);
 </script>";
 
-// echo $tab;
+?>
+
+<script src="assets/js/chart.js"></script>
